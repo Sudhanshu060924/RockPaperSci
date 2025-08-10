60c2740e0e613fd6a72abd6b90b3074d13ad4fdc
Vaishali Sonkar <sudhanshusingh0624@gmail.com>
2025-08-10 14:53:52 +0530

    first practice app

---
+++ app/src/main/java/su/dhanshu/rockpapersci/rockpapersci.kt
@@ -0,0 +1,38 @@
+package su.dhanshu.rockpapersci
+
+fun main(){
+    var computerChoice = ""
+    var userChoice = ""
+
+    println("enter your choice Rock , Paper , Scissors")
+    userChoice = readln()
+
+    var randomNumber = (1..3).random()
+    when (randomNumber) {
+        1 -> {
+            computerChoice = "Rock"
+        }
+        2 -> {
+            computerChoice = "Paper"
+        }
+        3 -> {
+            computerChoice = "Scissors"
+        }
+    }
+    println("The Computer choose:" + " " + computerChoice)
+    val winner = when {
+        userChoice == computerChoice  -> "Tie"
+        userChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+        userChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+        userChoice == "Paper" && computerChoice == "Rock" -> "Player"
+        else -> "Computer"
+
+    }
+    if (winner == "Tie"){
+        println("Its a tie")
+    }else {
+        println("The winner is :"+ " " + winner)
+    }
+
+
+}
